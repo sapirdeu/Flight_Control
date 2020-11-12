1a6e3f8b1c53a42dd50eba44266666bc5e6ad976
sapirdeu <sapiroosh.d@gmail.com>
2020-11-12 15:20:23 +0200

    download react + firebase

---
+++ flight_control/src/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAX9xm2uynGmk3ilIfYcjmdQKyjh8yDX5I",
+    authDomain: "flightcontrol-d32dc.firebaseapp.com",
+    databaseURL: "https://flightcontrol-d32dc.firebaseio.com",
+    projectId: "flightcontrol-d32dc",
+    storageBucket: "flightcontrol-d32dc.appspot.com",
+    messagingSenderId: "784136286846",
+    appId: "1:784136286846:web:ccac306f2ddf71004ede2f",
+    measurementId: "G-27XS9CCYMM"
+});
